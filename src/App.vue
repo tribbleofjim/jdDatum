e12192fb2514ebf848856acb8498a108647cfca9
tribbleofjim <lyifee@qq.com>
2021-03-03 15:32:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <echarts />
+    <ele-tree />
+  </div>
+</template>
+
+<script>
+/* eslint-disable quotes */
+/* eslint-disable semi */
+import Echarts from "./components/Echarts.vue";
+import EleTree from "./components/EleTree.vue";
+export default {
+  components: { Echarts, EleTree },
+  // eslint-disable-next-line comma-dangle
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
